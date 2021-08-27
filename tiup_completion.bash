636f5ee095d1dfcf8f0fe190c4017e38a142af5e
Yasuo Honda <yahonda@pingcap.com>
2021-08-27 15:01:28 +0900

    Created `tiup_completion.bash` to support tiup components
    make bash-autocompletion works for tiup components(ie. cluster, br).
    Current TiUP native bash-autocompletion does not support them
    
    Refer https://github.com/pingcap/tiup/pull/606
    
    - `cluster` and `br` compoments supported, others may be coming
    
    - Usage
    
    ```
    $ source /path/to/tiup_completion.bash
    $ tiup <TAB>
    bench           cdc             client          completion      dm              env             help            list            pd-recover      status          tidb            uninstall
    br              clean           cluster         ctl             dmctl           errdoc          install         mirror          playground      telemetry       tidb-lightning  update
    $ tiup c<TAB>
    cdc         clean       client      cluster     completion  ctl
    $ tiup cluster <TAB>
    audit        clean        destroy      display      enable       import       patch        reload       replay       scale-in     start        template
    check        deploy       disable      edit-config  help         list         prune        rename       restart      scale-out    stop         upgrade
    ```
    
    Thanks to
    https://stackoverflow.com/questions/17879322/how-do-i-autocomplete-nested-multi-level-subcommands

---
+++ tiup_completion.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+_tiup()
+{
+  local cur prev
+
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+
+  case ${COMP_CWORD} in
+    1)
+      COMPREPLY=( $(compgen -W "bench br cdc client cluster ctl dm dmctl errdoc pd-recover playground tidb tidb-lightning \
+                            clean completion env help install list mirror status telemetry uninstall update" -- $cur) )
+      ;;
+    2)
+      case ${prev} in
+        cluster)
+          COMPREPLY=($(compgen -W "check deploy start stop restart scale-in scale-out destroy clean upgrade display prune \
+                      list audit import edit-config reload patch rename enable disable replay template help" -- $cur) )
+          ;;
+        br)
+          COMPREPLY=($(compgen -W "backup help restore" -- $cur) )
+          ;;
+      esac
+      ;;
+    3)
+      COMPREPLY=()
+      ;;
+    esac
+}
+
+complete -F _tiup tiup
